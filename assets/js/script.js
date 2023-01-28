88f57bc5d4796db7ee48cee474387bae8cd993e3
Claudio Pinna <claudiopinna@live.com>
2023-01-28 20:19:06 +0100

    CSS UPDATED cleaned code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,73 @@
+//MY FAVOURITE SONGS//
+fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=muse`)
+  .then(function (data) {
+    console.log(`try`, data);
+
+    if (data.ok) {
+      return data.json();
+    }
+  })
+  .then(function (data) {
+    let albums = data.data;
+    console.log(albums);
+    let selectedIds = [13636688, 3590194, 3590186, 3599714];
+    let filteredSongs = albums.filter((song) => selectedIds.includes(song.id));
+    let card = document.getElementById("container-fav-songs");
+    filteredSongs.forEach((song) => {
+      card.innerHTML += `
+      <div class="col-5 col-sm-4 col-md-3 d-flex">
+      <div class="card my_card" style="width: 18rem">
+        <img
+          src="${song.album.cover_big}"
+          class="card-img-top"
+          alt="${song.album.title} pic"
+          style="height: 65%"
+        />
+        <div class="card-body d-flex flex-column justify-content-between">
+          <h5 class="card-title" id="title">${song.title}</h5>
+          <p class="card-text">${"Album: " + song.album.title}</p>
+          <p class="card-text">${"Artist: " + song.artist.name}</p>
+        </div>
+      </div>
+    </div>
+    `;
+    });
+  });
+
+//MY FAVOURITE SONG//
+fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=muse`)
+  .then(function (data) {
+    console.log(`try`, data);
+
+    if (data.ok) {
+      return data.json();
+    }
+  })
+  .then(function (data) {
+    let albums = data.data;
+    console.log(albums);
+    let selectedIds = [13636688];
+    let filteredSongs = albums.filter((song) => selectedIds.includes(song.id));
+    let card = document.getElementById("container-fav-song");
+    filteredSongs.forEach((song) => {
+      card.innerHTML += `
+        <div>
+          <img
+            src="${song.album.cover_big}"
+            class="card-img img-fluid rounded-start"
+            alt="${song.album.title}"
+          />
+        <div class="my_card_text">
+          <h5 class="card-title">${song.artist.name} : ${song.title}</h5>
+          <p class="card-text">${song.album.title}</p>
+          <p class="card-text"></p>
+        </div>
+      </div>
+`;
+    });
+  });
+
+//MY FAVOURITE ALBUMS CAROUSEL//
 fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=muse`)
   .then(function (data) {
     console.log(`try`, data);
@@ -195,9 +265,61 @@ fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=aretha`)
       </h5>
       <p>
       ${song.artist.name}
+      ${song.rank}
       </p>
     </div>
     
       `;
     });
   });
+
+//ALERT BUTTON//
+
+//RAGIONAMENTI VARI DA CANCELLARE!!!//
+
+/*DEVO CREARE UN'ARRAY CHE CONTIENE TUTTE LE CANZONI INDIVIDUATE IN PRECEDENZA
+FINO AD ORA LE HO SELEZIONATE TRAMITE ID FACENDO PRIMA LA RICHIESTA USANDO IL FETCH AL JSON REMOTO
+
+IN TEORIA HO GIA ACCESSO A TUTTO IL JSON PERO' COME FACCIO A PRENDERE IL RANK: POSSO FARLO TRAMITE SEMPLICE RICHIESTA
+USANDO ${song.rank} SU OGNI CANZONE... ho stabilito che funziona..
+
+COME PESCARLO DA OGNI CANZONE E CREARE UN ARRAY CHE LI CONTIENE TUTTI?
+COME FACCIO A CREARE L'ARRAY SE HO TUTTE LE FUNZIONI(VARIABILI) SEPARATE? [const array  = () => {....]*/
+
+const arrayDiTitoli = () => {
+  let p = document.querySelectorAll("p");
+  let titoli = [];
+  p.forEach((song) => {
+    titoli.push({
+      titolo: song.innerText,
+      rank: Number(song.getAttribute("song.rank")),
+    });
+  });
+  return titoli;
+};
+
+//codice bootrap per alert : //
+/*
+
+const alertPlaceholder = document.getElementById("liveAlertPlaceholder");
+
+const alert = (message, type) => {
+  const wrapper = document.createElement("div");
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    "</div>",
+  ].join("");
+
+  alertPlaceholder.append(wrapper);
+};
+
+const alertTrigger = document.getElementById("liveAlertBtn");
+if (alertTrigger) {
+  alertTrigger.addEventListener("click", () => {
+    alert("Nice, you triggered this alert message!", "success");
+  });
+} */
+
+//MODAL BUTTON//
